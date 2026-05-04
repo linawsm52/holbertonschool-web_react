83d66ec0f8d359d85317781e17c9baca56e55947
linawsm52 <lenaexo0@gmail.com>
2026-05-04 23:00:21 +0300

    new file

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -11,10 +11,6 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
-}
-
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -31,10 +27,45 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
 const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
   return `${firstName.charAt(0)}. ${lastName}`;
 };
 
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
 console.log(teacher3);
 console.log(director1);
 console.log(printTeacher("John", "Doe"));
+
+const student = new StudentClass("Lenae", "H");
+console.log(student.displayName());
+console.log(student.workOnHomework());
