2e1de57da4cbcbf4e803e731bc16b8331c1f25a5
linawsm52 <lenaexo0@gmail.com>
2026-05-04 22:55:24 +0300

    new file

---
+++ TypeScript/task_1/dist/bundle.js
@@ -0,0 +1,18 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!********************!*\
+  !*** ./js/main.ts ***!
+  \********************/
+
+var teacher3 = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+};
+console.log(teacher3);
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7OztBQVNBLElBQU0sUUFBUSxHQUFZO0lBQ3hCLFNBQVMsRUFBRSxNQUFNO0lBQ2pCLGdCQUFnQixFQUFFLEtBQUs7SUFDdkIsUUFBUSxFQUFFLEtBQUs7SUFDZixRQUFRLEVBQUUsUUFBUTtJQUNsQixRQUFRLEVBQUUsS0FBSztDQUNoQixDQUFDO0FBRUYsT0FBTyxDQUFDLEdBQUcsQ0FBQyxRQUFRLENBQUMsQ0FBQyIsInNvdXJjZXMiOlsid2VicGFjazovL3R5cGVzY3JpcHRfZGVwZW5kZW5jaWVzLy4vanMvbWFpbi50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJpbnRlcmZhY2UgVGVhY2hlciB7XHJcbiAgcmVhZG9ubHkgZmlyc3ROYW1lOiBzdHJpbmc7XHJcbiAgcmVhZG9ubHkgbGFzdE5hbWU6IHN0cmluZztcclxuICBmdWxsVGltZUVtcGxveWVlOiBib29sZWFuO1xyXG4gIHllYXJzT2ZFeHBlcmllbmNlPzogbnVtYmVyO1xyXG4gIGxvY2F0aW9uOiBzdHJpbmc7XHJcbiAgW2tleTogc3RyaW5nXTogYW55O1xyXG59XHJcblxyXG5jb25zdCB0ZWFjaGVyMzogVGVhY2hlciA9IHtcclxuICBmaXJzdE5hbWU6ICdKb2huJyxcclxuICBmdWxsVGltZUVtcGxveWVlOiBmYWxzZSxcclxuICBsYXN0TmFtZTogJ0RvZScsXHJcbiAgbG9jYXRpb246ICdMb25kb24nLFxyXG4gIGNvbnRyYWN0OiBmYWxzZSxcclxufTtcclxuXHJcbmNvbnNvbGUubG9nKHRlYWNoZXIzKTtcclxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
